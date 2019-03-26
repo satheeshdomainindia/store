bfe6dee540051cd5e35f7ac36e19124887195773
admin <satheesh@domainindia.org>
2019-03-26 23:12:38 +0530

    add category

---
+++ app/Category.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $guarded = [
+        'id', 
+    ];
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id', 'id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id', 'id');
+    }
+}
